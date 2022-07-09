0688854af35da3a210cf8c1a707dbefbc335ecbb
Jaylon <jaylonwingfield10@gmail.com>
2022-07-09 18:07:26 +0100

    Base project setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/app.js',
+        dom: './src/dom.js',
+        storage: './src/storage.js',
+        toDo: './src/tasks.js',
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    output: {
+        'filename': '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
