38f18c9efa7e5f81bd94ee77456f1ea037925066
Jaylon <jaylonwingfield10@gmail.com>
2022-07-11 15:50:11 +0100

    idGenerator function created to generate unique id's

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 import './css/style.css';
+
+export const idGenerator = (function () {
+    let projectCount = 0;
+    let taskCount = 0;
+
+    function generateTaskId() {
+        taskCount++;
+        return taskCount;
+    };
+
+    function generateProjectId() {
+        projectCount++;
+        return projectCount;
+    };
+})();
