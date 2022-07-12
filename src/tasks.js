5f1d81af35329bbd79e95240f8026a47d72506d3
Jaylon <jaylonwingfield10@gmail.com>
2022-07-12 13:46:56 +0100

    Function to push task to project array

--- src/tasks.js
+++ src/tasks.js
@@ -1,8 +1,14 @@
 export const project = (title, id) => {
+    const tasks = [];
+
+    function addTask(task) {
+        tasks.push(task);
+    };
     return {
         get getTitle() {return title},
         set setTitle(newTitle) {title = newTitle},
 
         get getId() {return id},
+        addTask,
     };
 };
