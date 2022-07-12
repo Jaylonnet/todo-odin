2af3865c69a9793fe00c9df03f9a8c87731e7e50
Jaylon <jaylonwingfield10@gmail.com>
2022-07-12 13:48:42 +0100

    Added eventListener to createProjectBtn

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import './css/style.css';
+import { project } from './tasks';
+import { displayProject } from './dom';
+
+const allProjects = [];
 
 export const idGenerator = (function () {
     let projectCount = 0;
@@ -13,4 +17,21 @@ export const idGenerator = (function () {
         projectCount++;
         return projectCount;
     };
+
+    return {
+        generateTaskId,
+        generateProjectId
+    };
 })();
+
+const createProjectBtn = document.querySelector('#create-project-btn');
+createProjectBtn.addEventListener('click', (event) => {
+    const form = document.forms['create-project-form'];
+
+    if (!form.elements['new-project-title'].value) { alert('Please ensure all fields are filled correctly.') }
+    else {
+        const newProject = project(form.elements['new-project-title'].value, idGenerator.generateProjectId());
+        allProjects.push(newProject);
+        displayProject(newProject.getTitle, newProject.getId, document.querySelector('#project-list ul'));
+    };
+});
